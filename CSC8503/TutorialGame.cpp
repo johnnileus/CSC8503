21e550df9bdc0cf35b87041c85c18079d4814858
gvidas <danieliusgvidas@gmail.com>
2024-12-09 10:10:11 +0000

    finished tutorial 5
    rotation doesnt work on collisions

--- CSC8503/TutorialGame.cpp
+++ CSC8503/TutorialGame.cpp
@@ -161,7 +161,8 @@ void TutorialGame::UpdateKeys() {
 		physics->UseGravity(useGravity);
 	}
 	//Running certain physics updates in a consistent order might cause some
-	//bias in the calculations - the same objects might keep 'winning' the constraint
+	//bias in the calculations - the same objects might keep 'winning' the 
+// 
 	//allowing the other one to stretch too much etc. Shuffling the order so that it
 	//is random every frame can help reduce such bias.
 	if (Window::GetKeyboard()->KeyPressed(KeyCodes::F9)) {
@@ -266,8 +267,11 @@ void TutorialGame::InitWorld() {
 
 	InitMixedGridWorld(15, 15, 3.5f, 3.5f);
 
+
 	InitGameExamples();
 	InitDefaultFloor();
+	BridgeConstraintTest();
+
 }
 
 /*
@@ -551,4 +555,33 @@ void TutorialGame::MoveSelectedObject() {
 
 }
 
+void TutorialGame::BridgeConstraintTest() {
+	Vector3 cubeSize = Vector3(8, 8, 8);
 
+	float invCubeMass = 5; // how heavy the middle pieces are
+	int numLinks = 10;
+	float maxDistance = 30; // constraint distance
+	float cubeDistance = 20; // distance between links
+
+	Vector3 startPos = Vector3(500, 500, 500);
+
+	GameObject* start = AddCubeToWorld(startPos + Vector3(0, 0, 0),
+		cubeSize, 0);
+	GameObject* end = AddCubeToWorld(startPos + Vector3((numLinks + 2) *
+		cubeDistance, 0, 0), cubeSize, 0);
+
+	GameObject* previous = start;
+
+	for (int i = 0; i < numLinks; ++i) {
+		GameObject* block = AddCubeToWorld(startPos + Vector3((i + 1) *
+			cubeDistance, 0, 0), cubeSize, invCubeMass);
+		PositionConstraint* constraint = new PositionConstraint(previous,
+			block, maxDistance);
+		world->AddConstraint(constraint);
+		previous = block;
+	}
+
+	PositionConstraint* constraint = new PositionConstraint(previous,
+		end, maxDistance);
+	world->AddConstraint(constraint);
+}
