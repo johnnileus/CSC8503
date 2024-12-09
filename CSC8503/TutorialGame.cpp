ab28facdca1f5825382162819e35ed53a79d9436
gvidas <danieliusgvidas@gmail.com>
2024-12-09 11:11:40 +0000

    finish tutorial 6, constraints

--- CSC8503/TutorialGame.cpp
+++ CSC8503/TutorialGame.cpp
@@ -563,7 +563,7 @@ void TutorialGame::BridgeConstraintTest() {
 	float maxDistance = 30; // constraint distance
 	float cubeDistance = 20; // distance between links
 
-	Vector3 startPos = Vector3(500, 500, 500);
+	Vector3 startPos = Vector3(50,100,0);
 
 	GameObject* start = AddCubeToWorld(startPos + Vector3(0, 0, 0),
 		cubeSize, 0);
